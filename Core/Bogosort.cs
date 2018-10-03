89910a901bf19ba50430a16f18ca329c0a1f38c6
Skye <Tarnthelos@gmail.com>
2018-10-03 13:25:30 -0400

    Initial commit

---
+++ Core/Bogosort.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sanity404Studios.Sorting
+{
+    public class Bogosort
+    {
+        Random _rand;
+
+        public Bogosort()
+        {
+            _rand = new Random();
+        }
+
+        /// <summary>
+        /// Sorts ints via bogo sort. Duplicate items will cause infinite loop.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public int[] Sort(int[] input)
+        {
+            int size = input.Length;
+            int iterations = 0;
+            int[] indexArr = new int[size];
+            int[] sorted = new int[size];
+
+            // 
+            for (int i = 0; i < size; i++)
+            {
+                sorted[i] = input[i];
+            }
+
+            List<int> usedNumbers = new List<int>();
+
+            bool beenSorted = false;
+            bool containsAll = false;
+
+            while (false == containsAll)
+            {
+                while (false == beenSorted)
+                {
+                    // Check if array is in order
+                    for (int i = 1; i <= size; i++)
+                    {
+                        if (size == i && sorted[i - 1] < sorted[i - 2])
+                        {
+                            break;
+                        }
+                        // Detects the case of being at the end of array
+                        else if ((size == i && sorted[i - 1] > sorted[i - 2]) || (size == i && sorted[i - 1] > sorted[i - 2]))
+                        {
+                            List<int> contains = new List<int>();
+                            contains.AddRange(sorted);
+
+                            if (true == ContainsAll(contains, input))
+                            {
+                                beenSorted = true;
+                                return sorted;
+                            }
+                        }
+                        else if (sorted[i] < sorted[i - 1])
+                        {
+                            break;
+                        }
+                        else if (sorted[i] == sorted[i - 1])
+                        {
+                            continue;
+                        }
+                    }
+
+                    // Creates the random array of indexes
+                    for (int i = 0; i < size; i++)
+                    {
+                        bool used = false;
+
+                        while (false == used)
+                        {
+                            int result = _rand.Next(size);
+
+                            if (false == usedNumbers.Contains(result))
+                            {
+                                usedNumbers.Add(result);
+                                indexArr[i] = result;
+                                used = true;
+                            }
+                        }
+                    }
+
+                    usedNumbers.Clear();
+
+                    // Assigns to the input array
+                    for (int i = 0; i < size; i++)
+                    {
+                        int r = input[indexArr[i]];
+                        sorted[i] = r;
+                    }
+                    iterations++;
+                    Console.WriteLine("Number of iterations: " + iterations);
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Checks to make sure that all numbers are there in the sorted array
+        /// </summary>
+        /// <param name="sortedLst"></param>
+        /// <returns></returns>
+        private bool ContainsAll(List<int> sortedLst, int[] arrToCheck)
+        {
+
+            for (int i = 0; i < sortedLst.Count; i++)
+            {
+                if (false == sortedLst.Contains(arrToCheck[i]))
+                {
+                    return false;
+                }
+                else if (true == sortedLst.Contains(arrToCheck[i]) && sortedLst.Count - 1 == i)
+                {
+                    return true;
+                }
+                else if (true == sortedLst.Contains(arrToCheck[i]))
+                {
+                    // Do nothing
+                }
+                else
+                {
+                    throw new Exception("Wut");
+                }
+            }
+
+            throw new Exception("Contains all is out of bounds");
+        }
+    }
+}
---
+++ UI/SortUI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Sanity404Studios.Sorting
+{
+    public class SortUI
+    {
+        public static void Main(string[] args)
+        {
+            Bogosort bSort = new Bogosort();
+            RandomNumberGenerator cryptRand = RandomNumberGenerator.Create();
+            Random rand = new Random();
+
+            int size = rand.Next(2, 10);
+            Console.WriteLine("Sorting " + size + " random numbers...");
+
+            byte[] data = new byte[size];
+            int[] byteData = new int[size];
+            int[] dataSorted;
+
+            cryptRand.GetBytes(data);
+
+            for (int i = 0; i < size; i++)
+            {
+                byteData[i] = data[i];
+            }
+
+
+            dataSorted = bSort.Sort(byteData);
+
+            for (int i = 0; i < dataSorted.Length; i++)
+            {
+                Console.WriteLine(dataSorted[i]);
+            }
+
+            Console.WriteLine("Sorted " + size + " random numbers.");
+            Console.ReadKey();
+        }
+    }
+}
